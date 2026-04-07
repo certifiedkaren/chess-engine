776562a010e1fb9cc5157dc39440e654d70fce3a
certifiedkaren <90162130+certifiedkaren@users.noreply.github.com>
2026-04-07 11:41:28 -0400

    added server for engine

--- server/engine.py
+++ server/engine.py
@@ -10,14 +10,12 @@ if stockfish_path is None:
 
 stockfish = Stockfish(path=stockfish_path)
 
-def get_best_moves(fen: str, depth: int, num : int = 3):
+def get_best_moves(fen: str, depth: int, num_results : int = 3):
   if (not stockfish.is_fen_valid(fen)):
     raise ValueError("fen is not valid")
   stockfish.set_fen_position(fen)
   stockfish.set_depth(depth)
-  return stockfish.get_top_moves(num)
+  return stockfish.get_top_moves(num_results)
 
 # check whose move it is and then when it returns centipawn: xxx
 # it will be that side which is doing better
-fen = "r5k1/p4pp1/2R4p/3p1b2/N4P2/1P4q1/P1P5/1K3R2 b - - 1 26"
-print(get_best_moves(fen, 18))
---
+++ server/server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+import engine
+
+class AnalyzeRequest(BaseModel):
+  fen : str = Field(description="FEN string for current chess position")
+  depth : int = Field(default=15, ge=1, le=25, description="Search depth for engine")
+  num_results : int = Field(default=3, ge=1, le=5, description="Number of top moves to return")
+
+app = FastAPI()
+@app.post("/analyze")
+async def analyze(data: AnalyzeRequest):
+  if not data.fen.strip():
+    raise HTTPException(status_code=400, detail="no fen received")
+  if data.depth > 25:
+    raise HTTPException(status_code=400, detail="enter a valid depth")
+  try:
+    best_moves = engine.get_best_moves(fen=data.fen, depth=data.depth, num_results=data.num_results)
+    return {"moves": best_moves}
+  except ValueError as e:
+    raise HTTPException(status_code=400, detail=str(e))
