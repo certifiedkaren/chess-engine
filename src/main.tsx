0ce4a051242fa18bc2354dc1514b7e4ebd911977
certifiedkaren <kevinnxie1@gmail.com>
2026-04-08 20:28:51 -0400

    finished basic sidebar component for pgn

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./App.css";
-import App from "./App.tsx";
+import "./ChessboardPanel.css";
+import ChessboardPanel from "./ChessboardPanel.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ChessboardPanel />
   </StrictMode>,
 );
